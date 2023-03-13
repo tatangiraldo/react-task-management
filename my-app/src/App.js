b31e1ece9244f8e68dc6fc6fad773f1af13576b5
tatan_giraldo <jhonnagira@gmail.com>
2023-03-13 17:15:11 -0500

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/*comment*/}
+        {/*comment*/} 
 
         {/*<GreetingF name='Tatan'/>*/}
         
